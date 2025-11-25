e2eaaef8652650163e918fd9ab8113760c60acd5
mehedi hasan <mhshohan04@gmail.com>
2025-11-25 23:11:28 +0600

    added single parcel get api

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/parcels/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/parcels', async(req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
