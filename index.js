21ff66b3e84b750434d93b32fdf5169a8371388b
mehedi hasan <mhshohan04@gmail.com>
2025-11-26 10:30:07 +0600

    coplete call checkout api

--- index.js
+++ index.js
@@ -67,13 +67,13 @@ async function run() {
     // PAYENT related api
     app.post("/create-checkout-session", async (req, res) => {
       const paymentInfo = req.body;
+      const amount = parseInt(paymentInfo.cost) * 100;
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
             price_data:{
               currency: 'USD',
-              unit_amount: 1500,
+              unit_amount: amount,
               product_data: {
                 name: paymentInfo.parcelName
               }
@@ -83,8 +83,13 @@ async function run() {
         ],
         customer_email: paymentInfo.senderEmail,
         mode: "payment",
+        metadata:  {
+          parcelId : paymentInfo.parcelId
+        },
         success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
       });
+      res.send({url: session.url})
     });
 
     // Send a ping to confirm a successful connection
