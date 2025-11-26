d6ebd1a1f38a8eb8f996474909cdc02a63f9df12
mehedi hasan <mhshohan04@gmail.com>
2025-11-26 10:21:05 +0600

    Create Stripe Checkout server api

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-require('dotenv').config();
-const port = process.env.PORT || 3000
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 // middleware
 app.use(express.json());
@@ -19,7 +20,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,45 +28,70 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db = client.db('motiar-store-db');
-    const parcelCollection = db.collection('parcels');
+    const db = client.db("motiar-store-db");
+    const parcelCollection = db.collection("parcels");
 
-    app.get('/parcels', async(req, res) => {
-       const query = {}
-       const {email} = req.query;
-       if(email){
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
         query.senderEmail = email;
-       }
-       const options = {sort: {createdAt: -1}}
-       const cursor = parcelCollection.find(query, options)
-       const result = await cursor.toArray();
+      }
+      const options = { sort: { createdAt: -1 } };
+      const cursor = parcelCollection.find(query, options);
+      const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
-    app.get('/parcels/:id', async(req, res) => {
+    app.get("/parcels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await parcelCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-    app.post('/parcels', async(req, res) => {
+    app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       parcel.createdAt = new Date();
       const result = await parcelCollection.insertOne(parcel);
       res.send(result);
-    })
+    });
 
-    app.delete('/parcels/:id', async (req, res) => {
+    app.delete("/parcels/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await parcelCollection.deleteOne(query);
       res.send(result);
-    })
+    });
+
+    // PAYENT related api
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data:{
+              currency: 'USD',
+              unit_amount: 1500,
+              product_data: {
+                name: paymentInfo.parcelName
+              }
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderEmail,
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -73,11 +99,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Motiar Store is shifting!')
-})
+app.get("/", (req, res) => {
+  res.send("Motiar Store is shifting!");
+});
 
 app.listen(port, () => {
-  console.log(`Motiar Store Server is listening on port ${port}`)
-})
+  console.log(`Motiar Store Server is listening on port ${port}`);
+});
